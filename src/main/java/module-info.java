10234c0a7dc4e91de6d9f3c33ad12aa559180817
Charlie <charlie@charlie.fr>
2021-04-06 18:10:21 +0200

    Update pom.xml and module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,19 +1,23 @@
 module swgoh.automation {
-  requires com.fasterxml.jackson.core;
-  requires com.fasterxml.jackson.databind;
+  // Modules
+  requires java.desktop;
   requires java.xml.bind;
   requires javafx.base;
   requires javafx.controls;
   requires javafx.fxml;
   requires javafx.graphics;
+  requires com.fasterxml.jackson.core;
+  requires com.fasterxml.jackson.databind;
+
+  // Automatic modules
   requires com.sun.jna;
   requires com.sun.jna.platform;
   requires sikulixapi;
   requires slf4j.api;
-  requires java.desktop;
   requires fuzzywuzzy;
   requires Saxon.HE;
 
+  // Openings for reflection
   opens com.charlie.swgoh.main;
   opens com.charlie.swgoh.javafx;
   opens com.charlie.swgoh.datamodel;
